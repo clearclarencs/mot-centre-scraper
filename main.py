e9bdd04b0cc5c870e905a1ab5f5034061a540ed9
clearclarencs <53274728+clearclarencs@users.noreply.github.com>
2024-01-30 17:23:28 +0000

    Added postcodes.txt, removed logging

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ postcode_string = 'class="postal-code">'
 def get_centers(postcode):
     try:
         page_text = requests.post("https://www.gov.uk/find-mot-course", data={"postcode": postcode}).text
-        print(1)
         return ",".join([f"{x.split('<')[0]}:{x.split(postcode_string)[1].split('<')[0]}" for x in page_text.split("govuk-heading-s govuk-!-display-inline\">")[1:]])
     except:
-        print(2)
         return "Unable to find centers"
 
 with open("postcodes.txt", "r") as r:
